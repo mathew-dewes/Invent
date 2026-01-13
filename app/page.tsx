7ad681e1c69c56a899e93e92ab1398c419f58944
Mathew Dewes <mathewdewes01@gmail.com>
2026-01-14 08:37:00 +1300

    Shadcn UI added with theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function HomePage(){
   return (
     <div>
       <div className="mt-20 mx-5">
       <h1 className="font-bold">Invent</h1>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam veritatis, officiis quod facere tenetur illum explicabo ab cum voluptatibus voluptatem.</p>
+      <Button variant={'default'}>Hello</Button>
       </div>
 
     </div>
